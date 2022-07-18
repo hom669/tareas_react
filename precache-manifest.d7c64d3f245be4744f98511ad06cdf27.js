dd8b1448638c390e2541ab1eb2eeace8d227d532
@Hom669 <h.oviedo@u2.com.co>
2022-07-17 19:01:37 -0500

    Updates

---
+++ precache-manifest.d7c64d3f245be4744f98511ad06cdf27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19fedaa487740b554fa3",
+    "url": "/tareas_react/static/css/main.52a4b004.chunk.css"
+  },
+  {
+    "revision": "19fedaa487740b554fa3",
+    "url": "/tareas_react/static/js/main.19fedaa4.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/tareas_react/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "81f9193b46c75c6d5405",
+    "url": "/tareas_react/static/js/2.81f9193b.chunk.js"
+  },
+  {
+    "revision": "436935f136318ead80d9",
+    "url": "/tareas_react/static/js/runtime~main.436935f1.js"
+  },
+  {
+    "revision": "172c23e3cdee7b7f3f34009ee7e70ca6",
+    "url": "/tareas_react/index.html"
+  }
+];
